4e84c1bdb7c4549923900535692831860e332c4a
Ozlem <ozlem.demirkiran3460@gmail.com>
2023-11-04 15:51:18 +0300

    gg

--- two-react-project/components/RightContent.jsx
+++ two-react-project/components/RightContent.jsx
@@ -1,11 +1,13 @@
 
 import Img2 from '../public/resimsag.svg';
+import Image from 'next/image';
+
 
 export default function RightContent() {
     return (
       <>
         <div className="bg-slate-600  w-1/2 p-2">
-          <img className="" src={<Img2/>} alt='karakter'/>
+          <Image className="" src={Img2} alt='karakter'/>
         </div>
       </>
     )
--- two-react-project/pages/index.js
+++ two-react-project/pages/index.js
@@ -1,12 +1,13 @@
 import LeftContent from "@/components/LeftContent";
 import RightContent from "@/components/RightContent";
 import Img1 from '../public/resimsol.svg';
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>  
       <div className="bg-black h-screen">
-      <img className='absolute left-3 bottom-1/4 p-2 bg-orange-400' src={<Img1/>} alt='yildiz'/>
+      <Image className='absolute left-3 bottom-1/4 p-2 bg-orange-400' src={Img1} alt='yildiz'/>
         
         <div className="flex justify-center items-center bg-white rounded-3xl h-screen p-2">
           <LeftContent/>
